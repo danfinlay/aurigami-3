6392386c23da13ba05486c7cfd8c13c7d890e0d1
Dan Finlay <dan@danfinlay.com>
2021-01-22 17:26:26 -0800

    Add binary blob conversion code

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,3 @@
-console.log('registering')
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 
 document.addEventListener('DOMContentLoaded',() => {
@@ -16,11 +15,33 @@ function newConvo () {
     console.log(stream);
     const audioCtx = new AudioContext();
     const source = audioCtx.createMediaStreamSource(stream);
-
     debugger;
+    const mediaRecorder = new MediaRecorder(sourceStream);
+    debugger;
+    const data = [];
+
+    mediaRecorder.ondataavailable = e => e.data.size && data.push(e.data);
+    mediaRecorder.start();
+    mediaRecorder.onstop = () => process(data);
+
     // Passthrough
     source.connect(audioCtx.destination);
   });
 }
 
 
+function process(data) {
+    const blob = new Blob(data);
+
+    convertToArrayBuffer(blob)
+        .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+        .then(play);
+}
+
+function convertToArrayBuffer(blob) {
+    const url = URL.createObjectURL(blob);
+
+    return fetch(url).then(response => {
+        return response.arrayBuffer();
+    });
+}

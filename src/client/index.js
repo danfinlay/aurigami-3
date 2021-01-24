6ca5d31c3e4ac88e49fe9162f6be705db3fdaee4
Dan Finlay <dan@danfinlay.com>
2021-01-23 18:38:48 -0800

    Make recording into chunked async iterator

--- src/client/index.js
+++ src/client/index.js
@@ -1,41 +1,64 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioCtx = new AudioContext();
+let controller = {};
 
 document.addEventListener('DOMContentLoaded',() => {
-  console.log('loaded');
+
+console.log('loaded');
   const newConvoButton = document.querySelector('button.newConvo');
   console.dir(newConvoButton);
   newConvoButton.addEventListener('click', newConvo);
-
 });
 
 function newConvo () {
   console.log(`Let's talk!`);
+
   navigator.mediaDevices.getUserMedia({audio: true})
   .then(function(stream) {
-    console.log(stream);
-    const audioCtx = new AudioContext();
-    const source = audioCtx.createMediaStreamSource(stream);
-    debugger;
-    const mediaRecorder = new MediaRecorder(sourceStream);
-    debugger;
-    const data = [];
 
-    mediaRecorder.ondataavailable = e => e.data.size && data.push(e.data);
-    mediaRecorder.start();
-    mediaRecorder.onstop = () => process(data);
+    const recorder = createRecorder({ stream });
 
-    // Passthrough
-    source.connect(audioCtx.destination);
   });
 }
 
+function createRecorder ({ chunkSize, stream } = { chunkSize: 5000 }) {
+  const source = audioCtx.createMediaStreamSource(stream);
+  let mediaRecorder = new MediaRecorder(sourceStream);
+  const data = [];
 
-function process(data) {
+  mediaRecorder.ondataavailable = (event) => {
+    event.data.size && data.push(event.data);
+  }
+  mediaRecorder.start(chunkSize);
+  mediaRecorder.onstop = () => process(data);
+  source.connect(audioCtx.destination);
+
+  return {
+    stop: () => {
+      mediaRecorder.stop();
+    },
+
+    async *[Symbol.asyncIterator]() {
+      // First clear the backlog
+      data.forEach((blob) => {
+        yield blob;
+      });
+
+      while (mediaRecorder.state === 'recording') {
+        yield new Promise((res) => {
+          mediaRecorder.ondataavailable = event => res(event.data);
+        })
+      }
+    }
+  }
+}
+
+function process(data, ctx) {
     const blob = new Blob(data);
 
     convertToArrayBuffer(blob)
         .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
-        .then(play);
+        .then((buff) => play(buff, ctx));
 }
 
 function convertToArrayBuffer(blob) {
@@ -44,4 +67,14 @@ function convertToArrayBuffer(blob) {
     return fetch(url).then(response => {
         return response.arrayBuffer();
     });
+}
+
+function play(audioBuffer) {
+    const sourceNode = audioCtx.createBufferSource();
+
+    sourceNode.buffer = audioBuffer;
+    sourceNode.detune.value = -300;
+
+    sourceNode.connect(audioContext.destination);
+    sourceNode.start();
 }
